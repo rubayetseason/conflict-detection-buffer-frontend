90abae2d27ca4f60550df1f80cf07b23686ffc60
rubayetseason <rubayetislamseason5@gmail.com>
2025-07-22 14:43:31 +0600

    [FEAT] --> layout setup

---
+++ src/app/(auth)/page.tsx
@@ -0,0 +1,7 @@
+export default function Auth() {
+  return (
+    <div>
+      <h1>This is auth page</h1>
+    </div>
+  );
+}
---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "../globals.css";
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "Conflict Detection Buffer | Dashboard",
+  description: "This is the dashboard page for the Conflict Detection Buffer",
+};
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
