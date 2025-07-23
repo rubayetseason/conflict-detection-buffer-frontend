3f1a828ebf33ca4c1b035fc8c8e029fe8372ea53
rubayetseason <rubayetislamseason5@gmail.com>
2025-07-23 18:01:40 +0600

    [FEAT] --> backend url updated

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -19,7 +19,8 @@ const processQueue = (error: any, token: string | null = null) => {
 };
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL:
+    "https://conflict-detection-buffer-backend.vercel.app/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
